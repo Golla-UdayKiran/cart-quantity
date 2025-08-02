458c7e22f2d228da9645be47fd1d1a65162f4a4f
Uday Kiran <uday@ceooftech.com>
2025-08-02 19:47:50 +0530

    implemented functions for code reusability

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 let cartQuantity = 0;
+
+function updateCartQuantity(change) {
+  if (cartQuantity + change > 10) {
+    alert('The cart is full');
+    return;
+  }
+  
+  if (cartQuantity + change < 0) {
+    alert('Not enough items in the cart');
+    return;
+  }
+  
+  cartQuantity += change;
+  console.log(`Cart quantity: ${cartQuantity}`);
+}
